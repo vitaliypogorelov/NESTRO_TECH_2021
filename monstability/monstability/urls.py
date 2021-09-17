31389aac4514d446c45dd40cfb6e1b26051237ad
Dmitriy Mishanin <dmitriymishanin@gmail.com>
2021-09-17 23:23:21 +0300

    Fix error #1

--- monstability/monstability/urls.py
+++ monstability/monstability/urls.py
@@ -21,8 +21,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-    path('auth/', include('djoser.urls.jwt')),
     path('api/v1/', include('visstability.urlsapi')),
     path('', include('visstability.urls')),
 ]
